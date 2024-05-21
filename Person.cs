a14dd69bf3074d1b3df0d96a137fbc4737bf92c1
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-21 12:00:09 -0400

    Up to tutorial 113

--- Person.cs
+++ Person.cs
@@ -1,6 +1,8 @@
 ﻿
 // Define a class
 
+using System.Security.Cryptography;
+
 public class Person 
 { 
     // Properties/Data Members
@@ -8,9 +10,29 @@ public class Person
     public string LastName { get; set; }
     public DateOnly DateofBirth { get; set; }
 
+    // Field member
+    private string _taxNumber;
+
     public void PrintFullName() 
     {
         Console.WriteLine($"{FirstName} {LastName}");
+    }
+
+    public void PrintInitials()
+    {
+        var firstInitial = FirstName[0];
+        var lastInitial = LastName[0];
+        Console.WriteLine($"{firstInitial} {lastInitial}");
+    }
+
+    public void GenerateTaxNumber()
+    {
+        _taxNumber = RandomNumberGenerator.GetInt32(100000, 9999999).ToString();
+    }
+
+    public string GetTaxNumber()
+    {
+        return _taxNumber;
     }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿Console.WriteLine("************ - Classes and Objects - *************");
 
+// Define an object of a type Person.
+Person person;
+
+Person baby = new Person();
+
+baby.LastName = "THompson";
+baby.FirstName = "Theresa";
+baby.DateofBirth = new DateOnly(2023, 10, 29);
+
+baby.PrintFullName();
+baby.PrintInitials();
+baby.GenerateTaxNumber();
+
+var taxNumber = baby.GetTaxNumber();
+
+Console.WriteLine(taxNumber);
